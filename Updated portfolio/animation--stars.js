61edf2efd96ae8eb81691d075d7b484db2477375
Bibiskop <76591489+Bibiskop@users.noreply.github.com>
2023-01-23 12:47:59 +0300

    Add files via upload

--- Updated portfolio/animation--stars.js
+++ Updated portfolio/animation--stars.js
@@ -3,7 +3,7 @@ let header = document.querySelector('header');
 
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
-    stars.style.left = value * 0.2 + 'px';
+    stars.style.left = value * 0.9 + 'px';
     stars.style.top = value * 0.5 + 'px';
     header.style.top = value * 0.5 + 'px';
 })
